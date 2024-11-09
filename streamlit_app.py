018c37998bd2f599b7790452bdbfd737aad348e2
SATYAM GUPTA <143061930+Satyamgupta2365@users.noreply.github.com>
2024-11-09 14:07:27 +0530

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,8 +26,8 @@ else:
 API_KEY_GROQ = "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"
 API_KEY_GENAI = "AIzaSyC4f-d-Igv6UWdHKoMgZcNfRTeQBFVgtUw"
 
-client = Groq(api_key=API_KEY_GROQ)
-genai.configure(api_key=API_KEY_GENAI)
+client = Groq(api_key="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")
+genai.configure(api_key="AIzaSyC4f-d-Igv6UWdHKoMgZcNfRTeQBFVgtUw")
 
 if not os.path.exists("temp"):
     os.makedirs("temp")
